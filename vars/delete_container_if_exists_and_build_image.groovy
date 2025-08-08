2ab8dc91750f1658bc639a6fe86f4ea0aa7da121
shivamFullStack111 <shvam12340987@gmail.com>
2025-08-08 21:16:46 +0530

    Update delete_container_if_exists_and_build_image.groovy

--- vars/delete_container_if_exists_and_build_image.groovy
+++ vars/delete_container_if_exists_and_build_image.groovy
@@ -1,6 +1,6 @@
 def call(String DOCKER_CONTAINER_NAME,String DOCKER_IMAGE_NAME){
   def containerName = sh(
-    script: "docker ps -a --name ${DOCKER_CONTAINER_NAME} --format '{{.Names}}'"
+    script: "docker ps -a --name ${DOCKER_CONTAINER_NAME} --format '{{.Names}}'",
     returnStdout: true
   ).trim()
   if (containerName) { 
