a8b1c7527ae0a124f3eac57b71bfe476195518ec
shivamFullStack111 <shvam12340987@gmail.com>
2025-08-08 20:10:07 +0530

    Update and rename delete_container_if_exists.groovy to delete_container_if_exists_and_build_image.groovy

--- vars/delete_container_if_exists_and_build_image.groovy
+++ vars/delete_container_if_exists_and_build_image.groovy
@@ -1,8 +1,10 @@
-def call(String DOCKER_CONTAINER_NAME){
+def call(String DOCKER_CONTAINER_NAME,DOCKER_IMAGE_NAME){
   if [ "$(docker ps -a --name ${DOCKER_CONTAINER_NAME} --format {{.Names}})"="${DOCKER_CONTAINER_NAME}" ]; then 
     echo "Deleting existing docker container..."
     docker rm -f ${DOCKER_CONTAINER_NAME}
   else
     echo "No existing container..."
   fi
+
+  docker build -t ${DOCKER_IMAGE_NAME}
 }
